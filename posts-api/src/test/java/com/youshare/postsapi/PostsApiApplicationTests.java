e10ba18a921ac6a79b08be28c85c90083ef5f037
Gabriel Soares <gabrielsa.gab@gmail.com>
2023-09-07 17:56:31 -0300

    posts-api initial commit

---
+++ posts-api/src/test/java/com/youshare/postsapi/PostsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youshare.postsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
