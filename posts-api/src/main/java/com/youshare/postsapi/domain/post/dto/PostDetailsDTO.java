0bc268c182976c4139dd1e43bc43a44fe54abfd8
Gabriel Soares <gabrielsa.gab@gmail.com>
2023-09-12 18:30:10 -0300

    Adding id field to the DTO

--- posts-api/src/main/java/com/youshare/postsapi/domain/post/dto/PostDetailsDTO.java
+++ posts-api/src/main/java/com/youshare/postsapi/domain/post/dto/PostDetailsDTO.java
@@ -4,8 +4,8 @@ import com.youshare.postsapi.domain.post.Post;
 
 import java.time.LocalDateTime;
 
-public record PostDetailsDTO(String content, Integer reactions, Integer comments, LocalDateTime postedDateTime, Long userId) {
+public record PostDetailsDTO(Long id, String content, Integer reactions, Integer comments, LocalDateTime postedDateTime, Long userId) {
     public PostDetailsDTO(Post post){
-        this(post.getContent(), post.getReactions(), post.getComments(), post.getPostedDateTime(), post.getUser().getId());
+        this(post.getId(), post.getContent(), post.getReactions(), post.getComments(), post.getPostedDateTime(), post.getUser().getId());
     }
 }
