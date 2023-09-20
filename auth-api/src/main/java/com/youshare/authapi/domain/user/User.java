6fa5496d8855831e4ce1f07844358e6c64c2c30c
Gabriel Soares <gabrielsa.gab@gmail.com>
2023-09-19 23:30:11 -0300

    Starting auth-api and adding User domain

---
+++ auth-api/src/main/java/com/youshare/authapi/domain/user/User.java
@@ -0,0 +1,23 @@
+package com.youshare.authapi.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "User")
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Boolean active;
+}
